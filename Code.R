0b4054bfe0f6dac4903487abc81514986c77cc7c
Daniela Frade <fradedaniela@yahoo.com>
2025-09-24 21:21:17 -0700

    prepared data for classification

--- Code.R
+++ Code.R
@@ -3,14 +3,28 @@ set.seed(123)
 library(tidyverse)
 library(themis)
 library(tidymodels)
+library(tidyr)
 
 sleep_data <- read_csv("data/Sleep_health_and_lifestyle_dataset.csv")
 head(sleep_data)
 
 sleep_data_clean <- sleep_data |>
+  select(-`Person ID`, -`Heart Rate`, -`Occupation`)|>
+  
   rename(Sleep_Disorder = `Sleep Disorder`)|>
+  rename(Gender = `Gender`)|>
+  rename(Age = `Age`)|>
+  rename(Sleep_Duration = `Sleep Duration`)|>
+  rename(Quality_of_Sleep = `Quality of Sleep`)|>
+  rename(Physical_Activity_Level = `Physical Activity Level`)|>
+  rename(Stress_Level = `Stress Level`)|>
+  rename(BMI_Category = `BMI Category`)|>
+  rename(Blood_Pressure = `Blood Pressure`)|>
+  rename(Daily_Steps = `Daily Steps`)|>
+  
   mutate(Sleep_Disorder = as_factor(Sleep_Disorder)) |>
   drop_na()
+
 
 
 counts_graph <- ggplot(sleep_data_clean, aes(x = Sleep_Disorder, fill = Sleep_Disorder)) + 
@@ -58,3 +72,22 @@ counts_graph <- ggplot(balanced_sleep, aes(x = Sleep_Disorder, fill = Sleep_Diso
     legend.text = element_text(size = 12)) +
   scale_fill_manual(values = c("darkorange", "darkblue", "darkred"))
 counts_graph
+
+balanced_sleep_tidy <- balanced_sleep |>
+  separate(Blood_Pressure, into = c("Systolic", "Diastolic"), sep = "/") |>
+  mutate(Systolic = as.numeric(Systolic), Diastolic = as.numeric(Diastolic)) |>
+  mutate(BMI_num = case_when(BMI_Category == "Underweight" ~ 1, 
+                             BMI_Category == "Normal"~ 2,
+                             BMI_Category == "Overweight"  ~ 3, 
+                             BMI_Category == "Obese" ~ 4))|>
+  
+  mutate(Gender_num = case_when(Gender == "Male" ~ 1, 
+                                Gender == "Female"~ 2,))|>
+  select(-BMI_Category, -Gender)
+
+
+sleep_split <- initial_split(balanced_sleep_tidy, prop = 0.70, strata = Sleep_Disorder)
+sleep_train <- training(sleep_split)
+sleep_test <- testing(sleep_split)
+
+
